2201cf5281426d30401f27d1edea293c87b96a7e
Brian Parker <brian.parker@hotmail.com.au>
2021-05-30 21:32:23 +1000

    code rub

--- ComponentViewer/Pages/Index.razor.cs
+++ ComponentViewer/Pages/Index.razor.cs
@@ -17,11 +17,11 @@ namespace ComponentViewer.Pages
         public (string OptionName, string typeName, Dictionary<string, object> parameters)[] components =
             new (string, string, Dictionary<string, object>)[]
             {
-                ("Survey", "ComponentViewer.Shared.SurveyPrompt", new Dictionary<string, object> { { "Title", "Leave your money on the fridge" } } ),
-                ("Fetch Data", "ComponentViewer.Pages.FetchData", new Dictionary<string, object>()),
-                ("Counter", "ComponentViewer.Pages.Counter", new Dictionary<string, object>() ),
-                ("Brian Parker", "ComponentViewer.Shared.StackFlare", new Dictionary<string, object>() ),
-                ("Bounce", "ComponentViewer.Shared.BounceDemo", new Dictionary<string, object>() )
+                ( "Survey", "ComponentViewer.Shared.SurveyPrompt", new Dictionary<string, object> { { "Title", "Leave your money on the fridge" } } ),
+                ( "Fetch Data", "ComponentViewer.Pages.FetchData", new Dictionary<string, object>()),
+                ( "Counter", "ComponentViewer.Pages.Counter", new Dictionary<string, object>() ),
+                ( "Brian Parker", "ComponentViewer.Shared.StackFlare", new Dictionary<string, object>() ),
+                ( "Bounce", "ComponentViewer.Shared.BounceDemo", new Dictionary<string, object>() )
             };
     }
 }
