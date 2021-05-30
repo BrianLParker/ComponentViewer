34b4a754ecdb3bc07f16d2b5fafb066e03db95f8
Brian Parker <brian.parker@hotmail.com.au>
2021-05-30 20:11:12 +1000

    wip

--- ComponentViewer/Shared/ComponentView.cs
+++ ComponentViewer/Shared/ComponentView.cs
@@ -10,11 +10,26 @@ namespace ComponentViewer.Shared
         [Parameter]
         public Type Type { get; set; }
 
+
+        [Parameter]
+        public string TypeString { get; set; }
+
+
         [Parameter]
         public Dictionary<string, object> Parameters { get; set; }
 
         protected override void BuildRenderTree(RenderTreeBuilder builder)
         {
+            if(Type is null)
+            {
+                if(string.IsNullOrWhiteSpace(TypeString))
+                {
+                    throw new Exception("You must use either Type or the TypeString paramater");
+                }
+
+                Type = Type.GetType(TypeString);
+            }
+
             if (Type is not null)
             {
                 builder.OpenComponent(1, Type);
